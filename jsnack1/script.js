ef364f26f77f8cb17d3b67f3bffd6cf041c7cccd
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-04 14:59:09 +0200

    first commit

---
+++ jsnack1/script.js
@@ -0,0 +1,44 @@
+/*
+jSnack 1
+Abbiamo un elenco utenti (nome, cognome, età).
+Trovare i minorenni e gli over 65
+*/
+
+const utenti = [
+  
+  {
+    firstname : Gianni,
+    lastname  : Fagianni,
+    age       : 54, 
+  },
+
+  {
+    firstname : Giannina,
+    lastname  : Fagianna,
+    age       : 15, 
+  },
+
+  {
+    firstname : Mario,
+    lastname  : Merola,
+    age       : 67, 
+  },
+
+  {
+    firstname : Siamo,
+    lastname  : Carichi,
+    age       : 30, 
+  },
+
+  {
+    firstname : Dominic,
+    lastname  : Toretto,
+    age       : 17, 
+  },
+
+  {
+    firstname : Luca,
+    lastname  : Abete,
+    age       : 70, 
+  },
+]
