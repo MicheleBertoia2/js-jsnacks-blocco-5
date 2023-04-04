3c39c90905b5a69864c0177a7a21c4438ccebb8e
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-04-04 15:03:58 +0200

    [solved snack 1]

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -7,38 +7,44 @@ Trovare i minorenni e gli over 65
 const utenti = [
   
   {
-    firstname : Gianni,
-    lastname  : Fagianni,
+    firstname : 'Gianni',
+    lastname  : 'Fagianni',
     age       : 54, 
   },
 
   {
-    firstname : Giannina,
-    lastname  : Fagianna,
+    firstname : 'Giannina',
+    lastname  : 'Fagianna',
     age       : 15, 
   },
 
   {
-    firstname : Mario,
-    lastname  : Merola,
+    firstname : 'Mario',
+    lastname  : 'Merola',
     age       : 67, 
   },
 
   {
-    firstname : Siamo,
-    lastname  : Carichi,
+    firstname : 'Siamo',
+    lastname  : 'Carichi',
     age       : 30, 
   },
 
   {
-    firstname : Dominic,
-    lastname  : Toretto,
+    firstname : 'Dominic',
+    lastname  : 'Toretto',
     age       : 17, 
   },
 
   {
-    firstname : Luca,
-    lastname  : Abete,
+    firstname : 'Luca',
+    lastname  : 'Abete',
     age       : 70, 
   },
 ]
+
+const minorenni = utenti.filter((utente) => (utente.age < 18));
+console.log(minorenni);
+
+const over65 = utenti.filter((utente) => (utente.age > 65));
+console.log(over65);
